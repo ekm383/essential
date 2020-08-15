5f58476d45ee8942d2597510114489260f3a9796
Eric Manongdo <ericmanongdo@Erics-MBP.hawaii.rr.com>
2020-08-14 17:14:48 -1000

    Contact Form added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,19 @@
 import React from "react"
 import Layout from "../components/layout"
+import Hero from "../components/header/Hero"
+import Banner from "../components/header/Banner"
+import Snapshot from "../components/report/Snapshot"
+import Contact from "../components/ContactForm"
 
 const contact = () => {
   return (
     <Layout>
-      <p>contact page</p>
+      <Hero>
+        <Banner>
+          <Snapshot hawaii="190 +34%" nation="190 +34%" world="190 +34%" />
+        </Banner>
+      </Hero>
+      <Contact />
     </Layout>
   )
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,17 @@
 import React from "react"
 import Layout from "../components/layout"
+import Hero from "../components/header/Hero"
+import Banner from "../components/header/Banner"
+import Snapshot from "../components/report/Snapshot"
 
 const login = () => {
   return (
     <Layout>
+      <Hero>
+        <Banner>
+          <Snapshot hawaii="190 +34%" nation="190 +34%" world="190 +34%" />
+        </Banner>
+      </Hero>
       <p>login</p>
     </Layout>
   )
